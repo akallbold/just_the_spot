e280811d644c4c5ae34e3677f272edeed2e491f8
AKB <a.kallenbornbolden@gmail.com>
2018-04-09 22:37:38 -0400

    Update Maps.js

--- MONY-frontend/src/Maps.js
+++ MONY-frontend/src/Maps.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
-
-
-let APIkey = "AIzaSyDOntKeg8k4VUKehDAFrH2GkGHr_mhJh28"
+let APIkey = ""
 
 
 class Maps extends React.Component {
@@ -13,13 +11,8 @@ class Maps extends React.Component {
     activeMarker:{}
   }
 
-  // componentDidMount(){
-  //   this.props.fetchCurrentPlaces(this.props.currentArticle)
-  // }
-
 
   createMarkers = () => {
-    // console.log("In createMarkers")
     return this.props.currentPlaces.map(place => {
        return <Marker
                 key={place.id}
@@ -63,7 +56,7 @@ class Maps extends React.Component {
                {lat:this.props.currentPlaces[0].latitude,lng:this.props.currentPlaces[0].longitude} : {lat:40.730610,lng:-73.935242}}
              style={{ width: "100%", height: "100%", position: "relative" }}
 
-             zoom={12}
+             zoom={10}
            >
               {this.createMarkers()}
               <InfoWindow
@@ -75,7 +68,6 @@ class Maps extends React.Component {
               </InfoWindow>
            </Map>
         </div>
-        {/* <button onClick= {()=>this.props.fetchSaveArticleToUser(this.props.currentArticle)} className="save-button" value="Save Map">Save Map</button> */}
       </div>
     )
   }
@@ -86,8 +78,3 @@ class Maps extends React.Component {
 export default GoogleApiWrapper({
 apiKey:APIkey
 })(Maps)
-// const WrappedContainers =
-// GoogleApiWrapper({apiKey:APIkey})
-// export default (Maps)
-// export const WrappedContainers = connect(mapStateToProps)(GoogleApiWrapper({
-// apiKey:APIkey}))(Maps)
